64ae20785afab26b90d9f0fec6698b4d772946b2
altescy <altescy@fastmail.com>
2020-01-15 23:43:18 +0900

    use pathlib for storage

--- logexp/storage.py
+++ logexp/storage.py
@@ -4,44 +4,49 @@ import typing as tp
 import contextlib
 import os
 import shutil
+from pathlib import Path
 
 
 class Storage:
-    def __init__(self, path: str) -> None:
-        if not os.path.isdir(path):
-            raise RuntimeError(f"storage directory not found: {path}")
-        self._path = path
+    def __init__(self, rootdir: Path) -> None:
+        if not rootdir.is_dir():
+            raise RuntimeError(f"storage directory not found: {rootdir}")
+        self._rootdir = rootdir
 
     @property
-    def path(self) -> str:
-        return os.path.abspath(self._path)
-
-    def _get_abspath(self, path: str) -> str:
-        return os.path.join(self.path, path)
+    def rootdir(self) -> Path:
+        return self._rootdir.absolute()
 
     def mkdirs(self, path: str, exist_ok: bool = False) -> None:
         _check_path(path)
-        os.makedirs(self._get_abspath(path), exist_ok=exist_ok)
+        os.makedirs(self.rootdir / path, exist_ok=exist_ok)
 
     def rmtree(self, path: str) -> None:
         _check_path(path)
-        shutil.rmtree(self._get_abspath(path))
+        shutil.rmtree(self.rootdir / path)
 
     def remove(self, path: str) -> None:
         _check_path(path)
-        os.remove(self._get_abspath(path))
+        os.remove(self.rootdir / path)
 
     @contextlib.contextmanager
     def open(self, path: str, *args, **kwargs) -> tp.Iterator:
         _check_path(path)
-        with open(self._get_abspath(path), *args, **kwargs)  as f:
+        with open(self.rootdir / path, *args, **kwargs)  as f:
             yield f
 
     def to_json(self) -> tp.Dict[str, tp.Any]:
         storage_dict = {
-            "path": self.path,
+            "rootdir": str(self.rootdir),
         }
         return storage_dict
+
+    @classmethod
+    def from_json(cls, storage_dict: tp.Dict[str, tp.Any]) -> Storage:
+        onestorage = Storage(
+            rootdir=Path(storage_dict["rootdir"]),
+        )
+        return onestorage
 
 
 def _check_path(path: str) -> None:
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+from pathlib import Path
 
 from logexp.storage import Storage
 
@@ -8,7 +9,8 @@ class TestStorage:
     @staticmethod
     def test_mkdirs():
         with tempfile.TemporaryDirectory() as tempdir:
-            storage = Storage(tempdir)
+            rootdir = Path(tempdir)
+            storage = Storage(rootdir)
             storage.mkdirs("foo")
 
             assert os.path.isdir(os.path.join(tempdir, "foo"))
@@ -16,25 +18,27 @@ class TestStorage:
     @staticmethod
     def test_rmtree():
         with tempfile.TemporaryDirectory() as tempdir:
-            storage = Storage(tempdir)
+            rootdir = Path(tempdir)
+            storage = Storage(rootdir)
             storage.mkdirs("foo/bar")
 
-            assert os.path.isdir(os.path.join(tempdir, "foo"))
-            assert os.path.isdir(os.path.join(tempdir, "foo/bar"))
+            assert (rootdir / "foo").is_dir()
+            assert (rootdir / "foo/bar").is_dir()
 
             storage.rmtree("foo")
 
-            assert not os.path.exists(os.path.join(tempdir, "foo"))
+            assert not (rootdir / "foo").exists()
 
     @staticmethod
     def test_open():
         with tempfile.TemporaryDirectory() as tempdir:
-            storage = Storage(tempdir)
+            rootdir = Path(tempdir)
+            storage = Storage(rootdir)
 
             with storage.open("foo.txt", "w") as f:
                 f.write("hello")
 
-            assert os.path.exists(os.path.join(tempdir, "foo.txt"))
+            assert (rootdir / "foo.txt").exists()
 
             with storage.open("foo.txt", "r") as f:
                 text = f.read()
