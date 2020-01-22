f622c481078051a05d8df407cbe19b8fcd3c2090
altescy <altescy@fastmail.com>
2020-01-22 15:45:33 +0900

    add test for params command

--- tests/cli/test_paramscommand.py
+++ tests/cli/test_paramscommand.py
@@ -27,3 +27,29 @@ class TestParamsCommand:
             ])
 
             args.func(args)
+
+    def test_runcommand_by_run(self):
+        module = "hello"
+        execution_path = "./example"
+        experiment_name = "my_experiment"
+        worker_name = "my_worker"
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            rootdir = Path(tempdir)
+
+            executor = Executor(
+                rootdir=rootdir,
+                module=module,
+                execution_path=execution_path,
+            )
+
+            experiment_id = executor.init(experiment_name)
+            runinfo = executor.run(experiment_id, worker_name)
+
+            args = self.parser.parse_args([
+                "params",
+                "-r", runinfo.uuid,
+                "-s", tempdir,
+            ])
+
+            args.func(args)
