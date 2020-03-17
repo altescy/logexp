73381bffe3cfee6b30997411da2b14f046f7ca70
altescy <altescy@fastmail.com>
2020-03-17 16:40:36 +0900

    add Storage

--- logexp/__init__.py
+++ logexp/__init__.py
@@ -1,4 +1,5 @@
 from logexp.experiment import Experiment
 from logexp.worker import BaseWorker
 from logexp.report import Report
+from logexp.storage import Storage
 from logexp.version import VERSION as __version__
