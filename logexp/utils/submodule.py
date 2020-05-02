3b9dbb171775e920ab746a95c7227b04de147539
altescy <altescy@fastmail.com>
2020-05-02 16:21:51 +0900

    add comment

--- logexp/utils/submodule.py
+++ logexp/utils/submodule.py
@@ -2,6 +2,8 @@ import importlib
 import pkgutil
 
 
+# This code is from
+# https://github.com/allenai/allennlp/blob/v0.9.0/allennlp/common/util.py#L308-L334
 def import_submodules(module_name: str) -> None:
     module = importlib.import_module(module_name)
     path = getattr(module, "__path__", [])
